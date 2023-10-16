681a07dc85c2c4de349909ccd86201e1cfacfc30
xya499 <302870488@qq.com>
2023-10-16 21:07:10 +0800

    添加全局打开抽屉功能

---
+++ src/components/common/123.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-button @click="close">123</el-button>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+
+  }
+},
+methods:{
+  close(){
+    console.log(this);
+    this.$root.onClose(123)
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
