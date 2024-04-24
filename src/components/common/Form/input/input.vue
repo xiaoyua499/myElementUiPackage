80ee90feb3e592edea29408aa5e48249eb556ece
小宇啊 <xya@MacBook-Air-M3.local>
2024-04-24 10:20:22 +0800

    封装form组件

--- src/components/common/Form/input/input.vue
+++ src/components/common/Form/input/input.vue
@@ -1,27 +1,64 @@
-<!-- Input.vue -->
 <template>
-  <el-input v-model="value" :disabled="disabled" :placeholder="placeholder" @blur="handleBlur"></el-input>
+  <div class="t-input">
+    <el-input
+      type="text"
+      :placeholder="placeholder"
+      clearable
+      @clear="clearValue"
+      :value="currentVal"
+      @input="value=>$emit('input',value)"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+    </el-input>
+  </div>
 </template>
-
 <script>
 export default {
-  props: {
-    value: String,
-    disabled: Boolean,
-    placeholder: String,
-  },
+  name: 'TInput',
   data() {
     return {
-      localValue: this.value, // 将 prop 的值赋给本地的 data 属性
-    };
+      currentVal: this.value
+    }
   },
-  methods: {
-    handleBlur() {
-      // 这里可以添加其他处理逻辑
-      this.$emit("blur", this.value);
+  props: {
+    isTip: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: [String, Number]
+    },
+    placeholder: {
+      type: String,
+      default: '请输入金额'
+    },
+    isShow: { // 是否显示尾缀
+      type: Boolean,
+      default: true
+    },
+    inputType: {
+      type: String,
+      default: 'currency'
     },
   },
-};
+  watch: {
+    value: {
+      handler(val) {
+        // console.log('组件watch', val)
+        this.currentVal = val
+      }
+    }
+  },
+  methods: {
+    clearValue() {
+      this.$emit('clearInput', this.currentVal)
+    },
+    toThousandsNoZero(num) {
+      return num ? ((isNaN(parseFloat(num.toString().replace(/,/g, ''))) ? 1 : parseFloat(num.toString().replace(/,/g, '')))).toFixed(2).toString().replace(/(\d)(?=(\d{3})+\.)/g, function ($0, $1) {
+        return $1 + ','
+      }) : ''
+    }
+  }
+}
 </script>
-
-<style></style>
