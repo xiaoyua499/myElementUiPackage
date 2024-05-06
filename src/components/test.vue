327ada1a3d850e2a91fa49baff56f0cc0eb4fd84
xiaoyua499 <302870488@qq.com>
2024-05-06 10:11:46 +0800

    feat:新增table组件dome

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,45 +1,28 @@
 <template>
-  <MyTable
-    :headers="headers"
-    :tableData="tableData"
-    :insertOrDelete="true"
-  ></MyTable>
+  <div>
+    <div v-for="(diff, index) in compareTexts(oldText, newText)" :key="index" :style="{ textDecoration: diff[0] === -1 ? 'line-through' : 'underline', color: diff[0] === 1 ? 'blue' : 'inherit' }">
+      {{ diff[1] }}
+    </div>
+  </div>
 </template>
 
 <script>
-import MyTable from './common/Table/index.vue'
+import { diff_match_patch } from 'diff-match-patch';
+
 export default {
-  components: {
-    MyTable
-  },
   data() {
     return {
-      headers: [
-        {
-          id: 1,
-          name: '列1'
-        },
-        {
-          id: 2,
-          name: '列2'
-        }
-      ],
-      tableData: [
-        {
-          '1': 123123,
-          '2': 123123
-        },
-        {
-          '1': 123123,
-          '2': 123123
-        }
-      ],
-    }
+      oldText: '哈哈哈哈这样不行',
+      newText: '哈哈这样123123行,哈哈'
+    };
   },
   methods: {
+    compareTexts(oldText, newText) {
+      const dmp = new diff_match_patch();
+      const diffs = dmp.diff_main(oldText, newText);
+      dmp.diff_cleanupSemantic(diffs);
+      return diffs;
+    }
   }
 }
 </script>
-
-<style>
-</style>
